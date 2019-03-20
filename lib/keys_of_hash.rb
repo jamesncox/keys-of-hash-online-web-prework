1613fa04be3d97e53dee7af58cd09efce2acb955
James Cox <jamesncox@yahoo.com>
2019-03-20 18:39:50 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,33 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*args)
+    map {|key, value| args.include?(value) ? key : nil }.compact
+  end
+end
+
+class Hash
+  def keys_of(*arguments)
+    array = []
+    self.each do |key, value|
+      arguments.each do |i|
+        if i == value
+        array.push(key)
+        end
+      end 
+    end
+    return array
+  end
+end 
+
+class Hash
+  def keys_of(*arguments)
+    arr = []
+    arguments.each do |arg|
+      self.each do |key, val|
+        if arg == val
+          arr.push(key)
+        end
+      end
+    end
+    arr
   end
 end

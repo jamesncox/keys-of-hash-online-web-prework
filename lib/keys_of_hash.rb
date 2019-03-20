fd5fc3d572e43c54132bb4886ec1d671a4699e3c
James Cox <jamesncox@yahoo.com>
2019-03-20 18:32:55 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,22 +1,22 @@
-# class Hash
-#   def keys_of(*args)
-#     map {|key, value| args.include?(value) ? key : nil }.compact
-#   end
-# end
+class Hash
+  def keys_of(*args)
+    map {|key, value| args.include?(value) ? key : nil }.compact
+  end
+end
 
-# class Hash
-#   def keys_of(*arguments)
-#     array = []
-#     self.each do |key, value|
-#       arguments.each do |i|
-#         if i == value
-#         array.push(key)
-#         end
-#       end 
-#     end
-#     return array
-#   end
-# end 
+class Hash
+  def keys_of(*arguments)
+    array = []
+    self.each do |key, value|
+      arguments.each do |i|
+        if i == value
+        array.push(key)
+        end
+      end 
+    end
+    return array
+  end
+end 
 
 class Hash
   def keys_of(*arguments)

2e889909b93f239484eeb7b883916a0b0c998bd2
James Cox <jamesncox@yahoo.com>
2019-03-20 18:31:04 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -5,3 +5,19 @@ class Hash
   end
 end
 
+class Hash
+  def keys_of(*arguments)
+    array = []
+    self.each do |key, value|
+      arguments.each do |i|
+        if i == value
+        array.push(key)
+        end
+      end 
+
+
+    end
+    return array
+  end
+  
+end 
